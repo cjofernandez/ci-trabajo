817829b2f9140126c2f3316205ac54ebe53342b3
cjofernandez <cjofernandez@poligran.edu.co>
2024-11-01 23:38:14 -0500

    Se crea proyecto base

---
+++ src/main/java/com/cjofernandez/msusuarios/MsUsuariosApplication.java
@@ -0,0 +1,11 @@
+package com.cjofernandez.msusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuariosApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuariosApplication.class, args);
+	}
+}
